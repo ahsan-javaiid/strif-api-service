e3737860e3de0dd9562d7821ad2f80ffc22dfaf0
ahsan-javaiid <ahsan.javaid@iovlabs.org>
2024-08-29 13:13:56 +0500

    feat: add validation and usd conversion

--- src/app/api/strif/[id]/route.tsx
+++ src/app/api/strif/[id]/route.tsx
@@ -39,18 +39,79 @@ const getVotes = async (address: string) => {
   return formattedBalance;
 }
 
+let RIF_VALUE = 0.078623;
+let LAST_UPDATED: any = new Date();
+let isFirstTime = true;
+
+const rifToUSD = async () => {
+  if (oneDayPast() || isFirstTime) {
+
+    // fetch latest value
+    try {
+      const link = `https://api.coingecko.com/api/v3/simple/price?ids=rif-token&vs_currencies=usd`;
+      const response = await fetch(link);
+      
+      if (response.ok && response.status === 200) {
+        const data =  await response.json();
+        const val = data["rif-token"];
+        if (val && val.usd) {
+          // update value
+          RIF_VALUE = val.usd;
+          LAST_UPDATED = new Date();
+          isFirstTime = false;
+          return Promise.resolve(RIF_VALUE);
+        } else {
+          return Promise.resolve(RIF_VALUE);
+        }
+      } else {
+        return Promise.resolve(RIF_VALUE);
+      }
+    } catch (e) {
+      return Promise.resolve(RIF_VALUE);
+    }
+  } else {
+    // use old value
+    return Promise.resolve(RIF_VALUE);
+  }
+}
+
+const oneDayPast = () => {
+  if (LAST_UPDATED) {
+    const currentDate = new Date();
+    const differenceInMilliseconds = currentDate.getTime() - LAST_UPDATED.getTime();
+    // 1 * 24 * 60 * 60 * 1000 is one day
+    const threeMonthsInMilliseconds = 1 * 24 * 60 * 60 * 1000;
+    // Check if the difference is greater than 1 day 
+    return differenceInMilliseconds > threeMonthsInMilliseconds;
+  }
+   
+  return false;
+}
+
+function isValidAddress(address: string) {
+  return /^0x[0-9a-fA-F]{40}$/.test(address)
+}
+
 export const GET = async (req: any, context: any) => { 
   const { params } = context;
 
-  const [balance, supply, votingPower] = await Promise.all([
+  if(!isValidAddress(params.id)) {
+    return NextResponse.json({
+      msg: 'Address is not valid!'
+    }, { status: 200, headers: corsHeaders });
+  }
+
+  const [balance, supply, votingPower, rifValue] = await Promise.all([
     balaneOfStRif(params.id),
     totalSupply(),
-    getVotes(params.id)
+    getVotes(params.id),
+    rifToUSD()
   ]);
 
   return NextResponse.json({
     data: {
       stackedBalance: parseFloat(balance),
+      stackedBalanceUSD: rifValue * parseFloat(balance),
       totalSupply: parseFloat(supply),
       votingPower: parseFloat(votingPower),
       network: 'testnet'
